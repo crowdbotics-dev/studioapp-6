aa224e4f745fa3359a2c1d665fb202ea052c7793
Crowdbotics <engineering@crowdbotics.com>
2023-03-09 19:28:09 +0000

    Update store.

--- store/mayank/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-const mayank = axios.create({
-  baseURL: "https://google.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
