5eaa8d6d3072f296e3a05dfb84dacca868ac22b7
Crowdbotics <engineering@crowdbotics.com>
2023-03-09 19:16:17 +0000

    Update store.

--- store/mayank/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { MAYANK_USERNAME, MAYANK_PASSWORD } from "react-native-dotenv"
-const mayank = axios.create({
-  baseURL: "http://ebizel.com/",
-  auth: { username: MAYANK_USERNAME, password: MAYANK_PASSWORD },
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
